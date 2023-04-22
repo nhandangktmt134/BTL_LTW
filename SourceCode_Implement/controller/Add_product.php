576fb3086a4c2d7a25df75606351c408e32fb093
Huynhle1833446 <huynhle1833446@hcmut.edu.vn>
2023-04-21 21:11:31 -0700

    add product and add user

---
+++ SourceCode_Implement/controller/Add_product.php
@@ -0,0 +1,8 @@
+<?php 
+    class Add_product extends Controller
+    {
+        function default() {
+            $this->view("add_product");
+        }
+    }
+?>
---
+++ SourceCode_Implement/controller/Add_user.php
@@ -0,0 +1,8 @@
+<?php 
+    class Add_user extends Controller
+    {
+        function default() {
+            $this->view("add_user");
+        }
+    }
+?>
